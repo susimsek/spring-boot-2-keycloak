3e0e7c12bfeb9c7e07d6465910344fbaa87f3a6d
suayb <suaybsimsek58@gmail.com>
2019-10-13 09:46:28 +0300

    add ex

---
+++ src/main/java/com/ex/SpringBoot2KeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2KeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2KeycloakApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ex/controllers/TokenController.java
@@ -0,0 +1,15 @@
+package com.ex.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+
+
+    @RequestMapping("/token")
+    public String getToken(){
+        return JWTUtil.getJWTToken();
+    }
+
+}
